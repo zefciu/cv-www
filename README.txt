22821cb3bdd3ce25abd139c8d480fa60db97981a
Szymon Pyżalski <zefciu@gmail.com>
2010-10-30 19:35:47 +0200

    Initial commit

---
+++ README.txt
@@ -0,0 +1,11 @@
+My CV as web application. Sort of demo of what I can do.
+
+Installation and Setup
+======================
+Install ``cv-www`` using easy_install::
+    easy_install cv-www
+Make a config file as follows::
+    paster make-config cv-www config.ini
+Tweak the config file as appropriate and then setup the application::
+    paster setup-app config.ini
+Then you are ready to go.
---
+++ cvwww/websetup.py
@@ -0,0 +1,18 @@
+"""Setup the cv-www application"""
+import logging
+
+import pylons.test
+
+from cvwww.config.environment import load_environment
+from cvwww.model.meta import Session, metadata
+
+log = logging.getLogger(__name__)
+
+def setup_app(command, conf, vars):
+    """Place any commands to setup cvwww here"""
+    # Don't reload the app if it was loaded under the testing environment
+    if not pylons.test.pylonsapp:
+        load_environment(conf.global_conf, conf.local_conf)
+
+    # Create the tables if they don't already exist
+    metadata.create_all(bind=Session.bind)
