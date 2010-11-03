9b1670ed435c9c1e2e1c2887267bd57f8a1de2cc
Szymon Pyżalski <zefciu@gmail.com>
2010-11-03 21:33:00 +0100

    Midwork

--- cvwww/public/js/main-tabs.js
+++ cvwww/public/js/main-tabs.js
@@ -1,9 +1,25 @@
+function loadData(ev) {
+	$.get(ev.target.url, null, onLoadData, 'json')
+}
+
+function setupBtns () {
+	var btns = $('.btn')
+	btns.button();
+	btns.each(function () {
+			this.url = this.getAttr('href');
+			this.removeAttr('href');
+			this.click(loadData);
+		});
+}
+
 $(function () {
 	$('#main-tabs').tabs({
 		load: function (ev, ui) {
 			if ($(ui.tab).hasClass('apply-accordion')) {
-				console.log($('.accordion'));
 				$('.accordion').accordion();
+			}
+			if ($(ui.tab).hasClass('apply-buttons')) {
+				setupBtns();
 			}
 		}
 	});
--- cvwww/templates/abilities.mako
+++ cvwww/templates/abilities.mako
@@ -3,6 +3,8 @@
 Umiejętności
 </%def>
 
+<div class="left-bar">
 % for cat in c.cats:
 	${h.link_to(cat.name, h.url_for(controller = 'abilities', action = 'get_abilities', slug = cat.slug), class_ = 'btn')}
 % endfor
+</div>
